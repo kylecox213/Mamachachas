6362e73a1b1db9a52f87d0ca374140cf01beb30c
Alex <kylecox213@gmail.com>
2020-08-25 23:26:56 -0400

    added interfaces for Category and Pie; added MockCategoryRepository

---
+++ Mamachachas/Models/IPieRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mamachachas.Models
+{
+	public interface IPieRepository
+	{
+		IEnumerable<Bread> AllBread { get; }
+		IEnumerable<Bread> BreadOfTheWeek { get; }
+		Bread GetBreadById(int breadId);
+	}
+}
