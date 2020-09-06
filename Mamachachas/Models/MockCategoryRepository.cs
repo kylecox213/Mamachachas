308525051bb72135533475a3dcfc6044a0f11556
Alex <kylecox213@gmail.com>
2020-09-06 03:19:46 -0400

    fixed errors in classes

---
+++ Mamachachas/Models/MockCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mamachachas.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId=1, CategoryName="Pan Delicioso", Description="Fresh , wholegrain, home-baked bread"},
+                new Category{CategoryId=2, CategoryName="Milnesas y Papas Fritas", Description=" Baked, golden-brown, thinly sliced cuts of beef"},
+                new Category{CategoryId=3, CategoryName="Empanadas de la Mamita", Description="Family recipe style Empanadas"}
+            };
+    }
+}
+
--- Mamachachas/Startup.cs
+++ Mamachachas/Startup.cs
@@ -17,6 +17,7 @@ namespace Mamachachas
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+			
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
