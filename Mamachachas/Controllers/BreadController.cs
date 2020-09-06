31f0a95a503c79972de92a91c915eb8b7b57f5a7
Alex <kylecox213@gmail.com>
2020-09-06 05:00:47 -0400

    fleshing out views and controllers

---
+++ Mamachachas/Controllers/BreadController.cs
@@ -0,0 +1,30 @@
+﻿using Mamachachas.Models;
+using Mamachachas.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mamachachas.Controllers
+{
+	public class BreadController : Controller
+	{
+		private readonly IBreadRepository _breadRepository;
+		private readonly ICategoryRepository _categoryRepository;
+
+		public BreadController(IBreadRepository breadRepository, ICategoryRepository categoryRepository)
+		{
+			_breadRepository = breadRepository;
+			_categoryRepository = categoryRepository;
+		}
+
+		public IActionResult List()
+		{
+			// ViewBag.CurrentCategory = "";
+
+			// return View(_breadRepository.AllBread);
+			BreadListViewModel breadListViewModel = new BreadListViewModel();
+			breadListViewModel.Bread = _breadRepository.AllBread;
+
+			breadListViewModel.CurrentCategory = "";
+			return View(breadListViewModel);
+		}
+	}
+}
---
+++ Mamachachas/ViewModels/BreadListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Mamachachas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mamachachas.ViewModels
+{
+	public class BreadListViewModel
+	{
+		public IEnumerable<Bread> Bread { get; set; }
+		public string CurrentCategory { get; set; }
+	}
+}
